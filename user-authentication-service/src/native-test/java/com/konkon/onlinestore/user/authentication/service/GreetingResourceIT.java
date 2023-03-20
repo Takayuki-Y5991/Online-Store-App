5a835e48241ecff2cc89fff38e27391d18dbc308
Takayuki-Y5991 <konkon.gitsune628@gmail.com>
2023-03-20 23:09:30 +0900

    first commit

---
+++ user-authentication-service/src/native-test/java/com/konkon/onlinestore/user/authentication/service/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package com.konkon.onlinestore.user.authentication.service;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
